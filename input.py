dbea26e8d46ecc18d8c9fbb437cc2a5d2b43570a
Milan Stepanov <milanstepanov89@gmail.com>
2024-11-14 11:00:47 +0100

    refactor YAML file processing

--- input.py
+++ input.py
@@ -3,13 +3,15 @@ from pathlib import Path
 from rosbags.highlevel import AnyReader
 from rosbags.typesys import Stores, get_typestore
 
+import os
 import yaml
+import json
+
 import numpy as np
 
 import cv2
 
 import argparse
-
 
 def extract_rgb(filename: str) -> tuple:
     """Return stereo pair from a BAG file _filename_."""
@@ -48,29 +50,66 @@ def extract_rgb(filename: str) -> tuple:
 
     return left, right
 
-
 class Meta:
     def __init__(self, path_to_yaml: str):
-        with open(path_to_yaml, ) as stream:
-            try:
-                meta = yaml.safe_load(stream)
-            except yaml.YAMLError as exc:
-                print(exc)
 
-        self.width = meta['image_width']
-        self.height = meta['image_height']
-        self.camera_name = meta['camera_name']
+        self.width, self.height = None, None
+        self.camera_name = None
 
-        self.camera_matrix = Meta.dict_to_np(meta['camera_matrix'])
-        self.distortion_coefficients = Meta.dict_to_np(meta['distortion_coefficients'])
-        self.rectification_matrix = Meta.dict_to_np(meta['rectification_matrix'])
-        self.projection_matrix = Meta.dict_to_np(meta['projection_matrix'])
+        self.camera_matrix = None
+        self.distortion_coefficients = None
+        self.rectification_matrix = None
+        self.projection_matrix = None
+
+        if path_to_yaml == "":
+            print(f"Path not provided. Metadata not set.")
+        else:
+            with open(path_to_yaml) as stream:
+                try:
+                    meta = yaml.safe_load(stream)
+
+                    self.width = meta['image_width']
+                    self.height = meta['image_height']
+                    self.camera_name = meta['camera_name']
+
+                    self.camera_matrix = Meta.dict_to_np(meta['camera_matrix'])
+                    self.distortion_coefficients = Meta.dict_to_np(meta['distortion_coefficients'])
+                    self.rectification_matrix = Meta.dict_to_np(meta['rectification_matrix'])
+                    self.projection_matrix = Meta.dict_to_np(meta['projection_matrix'])
+
+                except Exception as exc:
+                    print(f"Object not initialized.\n{exc}")
 
     @staticmethod
     def dict_to_np(dictionary: dict) -> np.array:
         return np.array(dictionary['data']).reshape((dictionary['rows'], dictionary['cols']))
 
+def metas_from_json(path_to_json: str):
+    if os.path.exists(path_to_json):
+        with open(path_to_json) as fp:
+            d = json.load(fp)
+            
+        left_meta = Meta("")
+        right_meta = Meta("")
 
+        left_meta.camera_matrix = np.array(d['left']['intrinsic']).reshape((3,3))
+        left_meta.distortion_coefficients = np.array(d['left']['distortion']).reshape((1,5))
+        left_meta.rectification_matrix = np.array(d['left']['rectification']).reshape((3,3))
+        left_meta.projection_matrix = np.array(d['left']['projection']).reshape((3,4))
+        left_meta.width = d['width']
+        left_meta.height = d['height']
+        
+        right_meta.camera_matrix = np.array(d['right']['intrinsic']).reshape((3,3))
+        right_meta.distortion_coefficients = np.array(d['right']['distortion']).reshape((1,5))
+        right_meta.rectification_matrix = np.array(d['right']['rectification']).reshape((3,3))
+        right_meta.projection_matrix = np.array(d['right']['projection']).reshape((3,4))
+        right_meta.width = d['width']
+        right_meta.height = d['height']
+
+        return left_meta, right_meta
+    
+    else:
+        raise FileNotFoundError(f"JSON file {path_to_json} not found.")
 
 if __name__ == "__main__":
 
